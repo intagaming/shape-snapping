2b0af8decf16124fc67a93fd568acbd8210c8a51
An Hoang <xuanan2001@pm.me>
2022-12-17 18:31:09 +0700

    render canvas

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -1,5 +1,22 @@
+import { useElementSize } from "usehooks-ts";
+
 const Canvas = () => {
-  return <div className="bg-orange-200 w-screen h-screen">Canvas</div>;
+  const [measuingDivRef, { width, height }] = useElementSize();
+
+  // TODO: rerender canvas on size change
+
+  return (
+    <div className="bg-gray-200 w-screen h-screen flex flex-col">
+      <div className="h-8 bg-green-400">Toolbar</div>
+
+      {/* Measuring div. Size will be used for canvas. */}
+      <div ref={measuingDivRef} className="flex-1">
+        <canvas width={width} height={height} className="bg-orange-200">
+          Canvas
+        </canvas>
+      </div>
+    </div>
+  );
 };
 
 export default Canvas;
